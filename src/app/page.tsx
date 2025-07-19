b34c6c99b47d60ce97b8fe70fe0991131d7ee8e8
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2025-07-19 23:20:52 +0300

    commit all supabase code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Dashboard } from "@/components/dashboard";
-import { supabase } from "@/lib/supabase";
 
 const user = {
   user_metadata: {
@@ -10,19 +9,19 @@ const user = {
 };
 
 export default function Home() {
-  const createUser = async () => {
-    const { data, error } = await supabase.from("users").insert({
-      // username: "ahmed_coder",
-      // college: "كلية الهندسة",
-      // year: "3",
-      // codeforces_handle: "ahmed_coder",
-      // leetcode_handle: "ahmed_coder",
-      name: "ahmed_coder",
-    });
-    if (error) console.log(error);
+  // const createUser = async () => {
+  //   const { data, error } = await supabase.from("users").insert({
+  //     // username: "ahmed_coder",
+  //     // college: "كلية الهندسة",
+  //     // year: "3",
+  //     // codeforces_handle: "ahmed_coder",
+  //     // leetcode_handle: "ahmed_coder",
+  //     name: "ahmed_coder",
+  //   });
+  //   if (error) console.log(error);
 
-    console.log(data);
-  };
+  //   console.log(data);
+  // };
 
   // return (
   //   <div className="min-h-screen bg-gray-50">
